4d6d799f703a910afe44c8fd4092083da4b9aa5e
Alex Comeau <17982529+alexm622@users.noreply.github.com>
2021-09-20 13:30:34 -0400

    matrix math now works

--- src/matrix.c
+++ src/matrix.c
@@ -1,15 +1,19 @@
 #include <matrix.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int ** add_matrix(int matrix1[3][3], int matrix2[3][3]){
-    int matrix_result[3][3] = {{0,0,0}, {0,0,0}, {0,0,0}};
+    //int matrix_result[3][3] = {{0,0,0}, {0,0,0}, {0,0,0}};
+    int* values = calloc(9, sizeof(int));
+    int** rows = malloc(3*sizeof(int*));
     printf("adding matricies\n");
     for(int i = 0; i < 3; i++){
         for(int j = 0; j < 3; j++){
-            matrix_result[i][j] = matrix1[i][j] + matrix2[i][j];
+            values[j] = matrix1[i][j] + matrix2[i][j];
         }
+        rows[i] = values;
     }
-    return matrix_result;
+    return rows;
 }
 
 void print_matrix(int ** matrix){
